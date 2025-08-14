2c10f9def3b139cea9c459411e03c6df1d7085b6
Yiğit Polat <yigpolatt@gmail.com>
2025-08-14 22:21:01 +0300

    Implemented sign up

---
+++ back/movie app/movie-app/src/main/java/com/example/movie/app/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.example.movie.app.controller;
+
+import com.example.movie.app.dto.SignUpDto;
+import com.example.movie.app.entity.User;
+import com.example.movie.app.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@Valid @RequestBody SignUpDto dto){
+        User user = authService.signup(dto);
+        return ResponseEntity.ok(user);
+    }
+}
---
+++ back/movie app/movie-app/src/main/java/com/example/movie/app/service/AuthService.java
@@ -0,0 +1,36 @@
+package com.example.movie.app.service;
+
+import com.example.movie.app.dto.SignUpDto;
+import com.example.movie.app.entity.User;
+import com.example.movie.app.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public User signup(SignUpDto dto){
+
+        if(userRepository.existsByUsername(dto.getUsername())){
+            throw new RuntimeException("Username already taken");
+        }
+
+        if(userRepository.existsByEmail(dto.getEmail())){
+            throw new RuntimeException("Email already in use");
+        }
+
+        User user = new User();
+        user.setUsername(dto.getUsername());
+        user.setEmail(dto.getEmail());
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+
+        return userRepository.save(user);
+    }
+}
