bfd05faaffce2d8459f1f2c9ec8c112a5e78bfc9
Yiğit Polat <yigpolatt@gmail.com>
2025-08-15 10:06:37 +0300

    Added filter chain

--- back/movie app/movie-app/src/main/java/com/example/movie/app/config/SecurityConfig.java
+++ back/movie app/movie-app/src/main/java/com/example/movie/app/config/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.example.movie.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -10,5 +13,17 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("auth/signup").permitAll()
+                        .anyRequest().authenticated());
+
+
+        return http.build();
     }
 }
--- back/movie app/movie-app/src/main/java/com/example/movie/app/dto/SignUpDto.java
+++ back/movie app/movie-app/src/main/java/com/example/movie/app/dto/SignUpDto.java
@@ -1,13 +1,9 @@
 package com.example.movie.app.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class SignUpDto {
 
     @NotBlank
